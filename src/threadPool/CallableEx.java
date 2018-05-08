36c04a61c819e7d013377d9eaf8a06d7b09e1405
nuthankumarkb <nuthankumarkb@NIBC2055.nousinfo.com>
2018-05-08 15:37:50 +0530

    kjkl

---
+++ src/threadPool/CallableEx.java
@@ -0,0 +1,16 @@
+package threadPool;
+
+import java.util.concurrent.Callable;
+
+public class CallableEx implements Callable<String>{
+	String obj="Nuts";
+
+	@Override
+	public String call() throws Exception {
+		return obj;
+	}
+
+	public String toString() {
+		return obj;
+	}
+}
